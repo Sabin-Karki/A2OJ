95f6a2257ff04816b18f9b04df6c45fe82b3bd99
sabin <sabinkarki064@gmail.com>
2026-01-17 08:14:53 +0545

    added edge case

--- CodeForces/SocialExperiment.java
+++ CodeForces/SocialExperiment.java
@@ -8,9 +8,12 @@ public class SocialExperiment{
           int n = scanner.nextInt();//n people
           int minimumDifference=0;
           //okay..so i have fully grasped the problem...now how can i approach is..n has say 5..now my first task is to distribute it 3.2 ..so the problem is sometime n could be 12...and it would need 4 team of 3 so 4 variable...sometime it maybe 5 so i need 2 variable to store 3n and 2n.......did tinkering in graph and found out if n is even md is 0 and if n is odd md is 1
-          if(n%2==0){
+          if(n==2||n==3){
+            minimumDifference=n;
+          }
+          else if(n%2==0){
            minimumDifference=0;
-          }else if(n%2!=0){
+          }else{
             minimumDifference=1;
         }        
     
